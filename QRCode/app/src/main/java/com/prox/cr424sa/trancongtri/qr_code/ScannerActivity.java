4cfe2d39fc0f2b3370d0dcf2b8a2923c4d0c3df2
captainprox <92608546+captainprox02@users.noreply.github.com>
2023-07-28 13:18:32 +0700

    siuuuu

--- QRCode/app/src/main/java/com/prox/cr424sa/trancongtri/qr_code/ScannerActivity.java
+++ QRCode/app/src/main/java/com/prox/cr424sa/trancongtri/qr_code/ScannerActivity.java
@@ -1,14 +1,19 @@
 package com.prox.cr424sa.trancongtri.qr_code;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.content.ContentProviderOperation;
 import android.content.Intent;
+import android.content.OperationApplicationException;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Handler;
+import android.os.RemoteException;
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.widget.TextView;
 
@@ -18,13 +23,21 @@ import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ScannerActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
 
     private static final int REQUEST_CAMERA_PERMISSION = 1;
+    private static final int REQUEST_WRITE_CONTACTS_PERMISSION = 2;
+
     private ZXingScannerView scannerView;
     private TextView textViewResult;
     private boolean isPopupShown = false;
     private boolean isProcessing = false;
+
+    private String scannedContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,13 +71,20 @@ public class ScannerActivity extends AppCompatActivity implements ZXingScannerVi
         if (!isProcessing) {
             isProcessing = true;
             String scanResult = result.getText();
+            scannedContent = scanResult;
+
 //        textViewResult.setText(scanResult);
             if (isURL(scanResult)) {
                 if (!isPopupShown) {
                     showURLOptions(scanResult);
                     isPopupShown = true;
                 }
-            } else {
+            } else if(isPhoneNumber(scanResult)){
+                if (!isPopupShown) {
+                    showPhoneResult(scanResult);
+                    isPopupShown = true;
+                }
+            }else{
                 if (!isPopupShown) {
                     showURLText(scanResult);
                     isPopupShown = true;
@@ -79,7 +99,7 @@ public class ScannerActivity extends AppCompatActivity implements ZXingScannerVi
             // Đếm ngược đã kết thúc, cho phép xử lý kết quả mới
             isProcessing = false;
             scannerView.resumeCameraPreview(this);
-        }, 3000); // 3000 ms = 3 giây
+        }, 3000);
     }
 
     private boolean isURL(String str) {
@@ -93,12 +113,12 @@ public class ScannerActivity extends AppCompatActivity implements ZXingScannerVi
                 .setTitle("QR Scan Result")
                 .setMessage(message)
                 .setPositiveButton("Open in Browser", (dialog, which) -> {
-                    // Thực hiện thao tác mở trình duyệt ở đây (nếu cần)
+                    //TODO: Thực hiện thao tác mở trình duyệt ở đây (nếu cần)
                     isPopupShown = false;
                     startCountdown();
                 })
                 .setNegativeButton("Copy Link", (dialog, which) -> {
-                    // Thực hiện thao tác sao chép đường dẫn ở đây (nếu cần)
+                    //TODO: Thực hiện thao tác sao chép đường dẫn ở đây (nếu cần)
                     isPopupShown = false;
                     startCountdown();
                 })
@@ -107,16 +127,194 @@ public class ScannerActivity extends AppCompatActivity implements ZXingScannerVi
     }
 
     private void showURLText(String text) {
+        Log.i("Content", text);
         String message = "Detected text:\n" + text + "\n\n";
         new AlertDialog.Builder(this)
                 .setTitle("QR Scan Result")
                 .setMessage(message)
                 .setPositiveButton("OK", (dialog, which) -> {
-                    // Thực hiện thao tác mở trình duyệt ở đây (nếu cần)
                     isPopupShown = false;
                     startCountdown();
                 })
                 .setCancelable(true)
                 .show();
     }
+
+    private void showPhoneResult(String content) {
+        // Hiển thị popup xác nhận lưu thông tin vào danh bạ
+        // Nếu người dùng đồng ý lưu, yêu cầu cấp quyền ghi danh bạ
+        new AlertDialog.Builder(this)
+                .setTitle("QR Scan Result")
+                .setMessage("Do you want to save this contact to your phone book?")
+                .setPositiveButton("Save", (dialog, which) -> requestWriteContactsPermission())
+                .setNegativeButton("Cancel", (dialog, which) -> finish())
+                .setCancelable(true)
+                .show();
+    }
+
+    private void requestWriteContactsPermission() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
+            // Đã có quyền, lưu thông tin vào danh bạ
+            saveToContacts();
+        } else {
+            // Xin cấp quyền WRITE_CONTACTS tại thời điểm chạy
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_CONTACTS}, REQUEST_WRITE_CONTACTS_PERMISSION);
+        }
+    }
+
+    private void saveToContacts() {
+        // Tạo một danh bạ mới
+        ArrayList<ContentProviderOperation> ops = new ArrayList<>();
+        int rawContactInsertIndex = ops.size();
+
+        ops.add(ContentProviderOperation.newInsert(ContactsContract.RawContacts.CONTENT_URI)
+                .withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, null)
+                .withValue(ContactsContract.RawContacts.ACCOUNT_NAME, null)
+                .build());
+
+        // Lưu tên vào danh bạ
+        ops.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, rawContactInsertIndex)
+                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
+                .withValue(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME, scannedContent) // Thay scannedContent bằng tên bạn muốn lưu
+                .build());
+
+        try {
+            // Thực hiện thêm thông tin vào danh bạ
+            getContentResolver().applyBatch(ContactsContract.AUTHORITY, ops);
+        } catch (RemoteException | OperationApplicationException e) {
+            e.printStackTrace();
+        }
+
+        // Hoàn thành và hiển thị thông báo thành công
+        new AlertDialog.Builder(this)
+                .setTitle("Success")
+                .setMessage("Contact has been saved to your phone book.")
+                .setPositiveButton("OK", (dialog, which) -> finish())
+                .setCancelable(false)
+                .show();
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_WRITE_CONTACTS_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Người dùng đã cho phép, lưu thông tin vào danh bạ
+                saveToContacts();
+            } else {
+                // Người dùng không cho phép, hiển thị thông báo và kết thúc
+                new AlertDialog.Builder(this)
+                        .setTitle("Permission Denied")
+                        .setMessage("You have denied the permission to save contact.")
+                        .setPositiveButton("OK", (dialog, which) -> finish())
+                        .setCancelable(false)
+                        .show();
+            }
+        }
+    }
+    public String extractPhoneNumberFromVCard(String vcard) {
+        // Biểu thức chính quy để tìm chuỗi TEL;TYPE=CELL:<số điện thoại>
+        String phonePattern = "TEL;TYPE=CELL:(\\d+)";
+
+        Pattern pattern = Pattern.compile(phonePattern);
+        Matcher matcher = pattern.matcher(vcard);
+
+        if (matcher.find()) {
+            // Lấy số điện thoại từ nhóm ngoặc trong biểu thức chính quy
+            return matcher.group(1);
+        } else {
+            return null;
+        }
+    }
+
+    public boolean isPhoneNumber(String vcard) {
+        // Biểu thức chính quy để tìm chuỗi TEL;TYPE=CELL:<số điện thoại>
+        String phonePattern = "TEL;TYPE=CELL:(\\d+)";
+
+        Pattern pattern = Pattern.compile(phonePattern);
+        Matcher matcher = pattern.matcher(vcard);
+
+        if (matcher.find()) {
+            // Lấy số điện thoại từ nhóm ngoặc trong biểu thức chính quy
+            return matcher.group(1).length() > 0;
+        } else {
+            return false;
+        }
+    }
+
+    public static void parseAndDisplayVCard(String vcardData) {
+        String namePattern = "N:(.*?);(.*?)\n";
+        String orgPattern = "ORG:(.*?)\n";
+        String cellPhonePattern = "TEL;TYPE=CELL:(.*?)\n";
+        String phonePattern = "TEL:(.*?)\n";
+        String faxPattern = "TEL;TYPE=FAX:(.*?)\n";
+        String addressPattern = "ADR:(.*?)\n";
+        String emailPattern = "EMAIL;TYPE=INTERNET:(.*?)\n";
+
+        Pattern namePat = Pattern.compile(namePattern);
+        Pattern orgPat = Pattern.compile(orgPattern);
+        Pattern cellPhonePat = Pattern.compile(cellPhonePattern);
+        Pattern phonePat = Pattern.compile(phonePattern);
+        Pattern faxPat = Pattern.compile(faxPattern);
+        Pattern addressPat = Pattern.compile(addressPattern);
+        Pattern emailPat = Pattern.compile(emailPattern);
+
+        Matcher nameMatcher = namePat.matcher(vcardData);
+        Matcher orgMatcher = orgPat.matcher(vcardData);
+        Matcher cellPhoneMatcher = cellPhonePat.matcher(vcardData);
+        Matcher phoneMatcher = phonePat.matcher(vcardData);
+        Matcher faxMatcher = faxPat.matcher(vcardData);
+        Matcher addressMatcher = addressPat.matcher(vcardData);
+        Matcher emailMatcher = emailPat.matcher(vcardData);
+
+        String name = "";
+        String org = "";
+        String cellPhone = "";
+        String phone = "";
+        String fax = "";
+        String address = "";
+        String email = "";
+
+        if (nameMatcher.find()) {
+            name = nameMatcher.group(2) + " " + nameMatcher.group(1);
+        }
+
+        if (orgMatcher.find()) {
+            org = orgMatcher.group(1);
+        }
+
+        if (cellPhoneMatcher.find()) {
+            cellPhone = cellPhoneMatcher.group(1);
+        }
+
+        if (phoneMatcher.find()) {
+            phone = phoneMatcher.group(1);
+        }
+
+        if (faxMatcher.find()) {
+            fax = faxMatcher.group(1);
+        }
+
+        if (addressMatcher.find()) {
+            address = addressMatcher.group(1);
+        }
+
+        if (emailMatcher.find()) {
+            email = emailMatcher.group(1);
+        }
+
+        // Hiển thị thông tin trong AlertDialog
+        String message = "Name: " + name + "\n" +
+                "Organization: " + org + "\n" +
+                "Cell-phone: " + cellPhone + "\n" +
+                "Phone: " + phone + "\n" +
+                "Fax: " + fax + "\n" +
+                "Address: " + address + "\n" +
+                "Email: " + email;
+        
+    }
+}
+
 }
